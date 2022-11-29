932fa4ec310cdbd7324b2ef1f4dec9f08ddfdfdc
Octoping925 <mooner925@gmail.com>
2022-11-29 17:18:11 +0900

    통합테스트 작업 중

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,12 +1,46 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { Util } from '../Util';
 
-/*
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('통합 테스트', () => {
+  const ANSWER = 'KOREA';
+  Util.pickRandomAnswer = jest.fn(() => ANSWER);
+
+  const screen = render(<App />);
+
+  describe('최초 생성 시 정상인지 체크', () => {
+    test('로고가 잘 떴는지 확인한다', () => {
+      const title = screen.getByText('Wordle');
+      expect(title).toBeInTheDocument();
+    });
+  
+    test('input이 잘 떴는지 확인한다', () => {
+      const input = screen.getByPlaceholderText('단어를 입력해주세요');
+      expect(input).toBeInTheDocument();
+    });
+  });
+
+  describe('단어 정상 입력 체크', () => {
+
+  });
+
+  describe('유효하지 않은 입력 체크', () => {
+
+  });
+
+  describe('재시작 버튼 클릭 체크', () => {
+    const restartButton = screen.getByText('재시작');
+    expect(restartButton).toBeInTheDocument();
+    restartButton.click();
+
+    // test('재시작 버튼을 누르면 화면의 표의 모든 내용이 초기화 된다', () => {
+    //   const table = container.querySelector('table');
+    // });
+
+    test('재시작 버튼을 누르면 input의 내용이 초기화 된다', () => {
+      const input = screen.getByPlaceholderText('단어를 입력해주세요');
+      expect(input.innerText).toBe('');
+    });
+  });
+
 });
-*/
-
-test('hi', () => expect(1).toBe(1))
---
+++ src/test/Row.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { Constants } from '../Config';
+import Row from '../Row';
+
+
+test('값이 들어있는 Row 렌더링', () => {
+    // given
+    const word = "KOREA";
+    const colorArr = [
+        Constants.GREEN, 
+        Constants.YELLOW, 
+        Constants.GRAY, 
+        Constants.GREEN, 
+        Constants.YELLOW
+    ];
+
+    // when
+    const { container } = render(<table><tbody><Row word={word} color={colorArr} /></tbody></table>);
+
+    // then
+    const grids = Array.from(container.firstChild.firstChild.children);
+
+    expect(grids.length).toEqual(word.length);
+    expect(grids.every((grid, idx) => grid.innerHTML === word[idx])).toBeTruthy();
+});
+
+test('값이 비어있는 Row 렌더링', () => {
+    // given
+    const word = ' '.repeat(Constants.WORD_LEN);
+    const colorArr = [
+        Constants.WHITE,
+        Constants.WHITE,
+        Constants.WHITE,
+        Constants.WHITE,
+        Constants.WHITE
+    ];
+
+    // when
+    const { container } = render(<table><tbody><Row word={word} color={colorArr} /></tbody></table>);
+
+    // then
+    const grids = Array.from(container.firstChild.firstChild.children);
+
+    expect(grids.length).toEqual(word.length);
+    expect(grids.every((grid, idx) => grid.innerHTML === word[idx])).toBeTruthy();
+})
